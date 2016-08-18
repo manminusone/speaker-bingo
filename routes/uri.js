37f5b158aac2ca7b2b930ebd97255dd594eb7bdb
James Allenspach <jima@legnog.com>
2016-08-17 22:14:05 -0500

    Starting to work on uri screens

--- routes/uri.js
+++ routes/uri.js
@@ -5,8 +5,17 @@ module.exports = (options) => {
 	var db = options.db;
 
 	router.get('/', function(req,res,next) {
-		res.render('index', { title: 'URI hell'});
+		res.render('index', { title: 'URI hell', message: ''});
 	});
+
+	router.get('/:uri', function(req,res,next) {
+		db.presentation.findByUriNoPwd(req.params.uri, function(err,presentation) {
+			if (presentation)
+				res.render('uri-hello', { uri: req.params.uri })
+			else
+				res.render('index', { title: 'URI hell', message: 'URI not found' });
+		});
+	})
 
 	return router;
 };
